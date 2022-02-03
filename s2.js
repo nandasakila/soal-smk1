490d01fa8ac2b5a89f5d7d4b56f55cf056558acf
nandasakila <nandasakilaipalmughni@gmail.com>
2022-02-03 13:46:21 +0700

    soal smk1

---
+++ s2.js
@@ -0,0 +1,10 @@
+function Panggil(bilangan1,bilangan2) {
+    if (bilangan1 + bilangan2)  {
+        console.log(true);
+    }  
+    else {
+        console.log(false);
+        }
+   }
+   
+   Panggil(30,18);
---
+++ s3.js
@@ -0,0 +1,9 @@
+function Panggilan(huruf1,huruf2) {
+    if(huruf1.length == huruf2.length){
+        console.log(true);
+    }
+    else{
+        console.log(false);
+    }
+}
+Panggilan(`AB`,`CD`);
